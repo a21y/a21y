f5b85d64d311d84317bf640b5faa26841efc8a42
ndrewcowley <ndrewcowley@gmail.com>
2021-11-06 23:00:32 +0200

    Add plausible

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,6 +39,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
+    'vue-plausible'
   ],
 
   // Content module configuration: https://go.nuxtjs.dev/config-content
